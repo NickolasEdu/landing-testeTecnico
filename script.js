9e0d99f7d5d8c350b99b8ec44477826713d512c7
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-04-05 21:18:43 -0300

    Started with the JS

---
+++ script.js
@@ -0,0 +1,47 @@
+const mainform = document.getElementById('mainForm')
+
+mainform.onsubmit = function(e) {
+
+    e.preventDefault()
+
+    const inputName = document.forms['mainForm']['userName']
+    if (!inputName.value) {
+        alert('Name error')
+    } else {
+        emailValidation()
+    }
+}
+
+
+function emailValidation() {
+    
+    const inputEmail = document.forms['mainForm']['userEmail']
+    
+    if (!inputEmail.value) {
+        // inputEmail.classList.add('Error')
+        alert('Email Error')
+    } else {
+        identifyValidation()
+    }
+}
+
+function identifyValidation() {
+    const inputCPF = document.forms['mainForm']['userCPF']
+    
+    if (!inputCPF.value) {
+        alert('CPF Error') 
+    } else {
+        genderValidation()
+    }
+}
+
+function genderValidation() {
+    const inputMaleGender = document.forms['mainForm']['userMale']
+    const inputFemaleGender = document.forms['mainForm']['userFemale']
+
+    if (!inputFemaleGender.value || !inputMaleGender.value) {
+        alert('Gender Error')
+    } else {
+        alert('ok')
+    }
+}
