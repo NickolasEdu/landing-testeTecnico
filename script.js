fed7957764fe38e9f1104de8f16971c848c9cf8c
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-04-07 21:07:33 -0300

    validation algorithm set

--- script.js
+++ script.js
@@ -2,50 +2,74 @@
 
 const mainform = document.getElementById('mainForm')
 
-mainform.onsubmit = function(e) {
+const inputName = document.forms['mainForm']['userName']
+const inputEmail = document.forms['mainForm']['userEmail']
+const inputCPF = document.forms['mainForm']['userCPF']
+const inputGender = document.forms['mainForm']['userGender']
+const helperMenssage = document.getElementById('helperText')
+const sucessTxt = document.getElementById('sucessText')
+
+
+mainform.onsubmit = function nameValidation(e) {
 
     e.preventDefault()
 
-    const inputName = document.forms['mainForm']['userName']
     if (!inputName.value) {
         inputName.classList.add('returnError')
+        errorText()
     } else {
         inputName.classList.remove('returnError')
+        defaultText()
         emailValidation()
     }
 }
 
 
 function emailValidation() {
-    
-    const inputEmail = document.forms['mainForm']['userEmail']
-    
+     
     if (!inputEmail.value) {
         inputEmail.classList.add('returnError')
+        errorText()
     } else {
         inputEmail.classList.remove('returnError')
+        defaultText()
         identifyValidation()
     }
 }
 
 function identifyValidation() {
-    const inputCPF = document.forms['mainForm']['userCPF']
     
     if (!inputCPF.value) {
-        inputCPF.classList.add('returnError') 
+        inputCPF.classList.add('returnError')
+        errorText()
     } else {
         inputCPF.classList.remove('returnError')
+        defaultText()
         genderValidation()
     }
 }
 
 function genderValidation() {
-    const inputMaleGender = document.forms['mainForm']['userMale']
-    const inputFemaleGender = document.forms['mainForm']['userFemale']
 
-    if (!inputFemaleGender.value || !inputMaleGender.value) {
-        alert('Gender Error')
+    if (!inputGender.value) {
+        errorText()
     } else {
-        alert('ok')
+        defaultText()
+        sucessText()
     }
+}
+
+function errorText() {
+    helperMenssage.classList.add('helper-text')
+    helperMenssage.classList.remove('hidden')
+}
+
+function defaultText() {
+    helperMenssage.classList.remove('helper-text')
+    helperMenssage.classList.add('hidden')
+}
+
+function sucessText() {
+    sucessTxt.classList.remove('hidden')
+    sucessTxt.classList.add('sucess-text')
 }
